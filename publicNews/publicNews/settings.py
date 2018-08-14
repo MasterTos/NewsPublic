57ee213b4a5ce6e58cdc12ae49a5e02fe33c8aa6
MasterTos <MasterTos@yahoo.com>
2018-08-14 18:46:15 +0700

    change setting

--- publicNews/publicNews/settings.py
+++ publicNews/publicNews/settings.py
@@ -243,18 +243,11 @@ CKEDITOR_CONFIGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-STATIC_ROOT = "/var/www/public/static/"
 
 MEDIA_URL = '/media/'
-MEDIA_DIRS = [
-    os.path.join(BASE_DIR, "media")
-]
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-# MEDIA_ROOT = "/var/www/public/media/"
+MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'media'))
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
