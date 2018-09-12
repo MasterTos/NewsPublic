8f9c3ec494dcea988973f5513c4d88bbf22288d0
wararom <35321135+wararom@users.noreply.github.com>
2018-09-12 12:15:49 +0700

    Update settings.py

--- publicNews/publicNews/settings.py
+++ publicNews/publicNews/settings.py
@@ -84,13 +84,13 @@ WSGI_APPLICATION = 'publicNews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'postgres',
-        # 'USER': 'postgres',
-        # 'HOST': 'db_publicnews',
-        # 'PORT': 5432,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db_publicnews',
+        'PORT': 5432,
     }
 }
 
